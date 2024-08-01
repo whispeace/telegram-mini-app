94b88d38123b8a60305ce906136656cc1b71814f
Ed Shamshurin <e.shamshurin@internet.ru>
2024-08-01 09:37:06 +0300

    chore: Update deployment workflow and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './telegram-mini-app',
   plugins: [
     vue(),
     vueDevTools(),
